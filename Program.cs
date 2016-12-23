e7f23dd9e154e41f6a41c8245b68a67f681d3660
shaty <shaty@DESKTOP-R10MJOM>
2016-12-24 02:36:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CleanObjAndBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirs = Directory.GetDirectories(".\\", "*", SearchOption.AllDirectories).Where(_ => Regex.IsMatch(_, "(bin|obj)")).ToArray();
+
+            foreach (var dir in dirs)
+            {
+                var files = Directory.GetFiles(dir, "*", SearchOption.AllDirectories);
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        Console.WriteLine(file);
+                        File.Delete(file);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+
+            Console.WriteLine("---");
+            Console.ReadLine();
+        }
+    }
+}
